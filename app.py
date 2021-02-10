b72cefb321368db981a96292fa60bbd7d577a6cc
Willem Essenstam <wessenstam@gmail.com>
2021-02-10 11:38:50 +0100

    Updated with the last feedback form the GTS sync session...

--- app.py
+++ app.py
@@ -15,39 +15,50 @@ from wtforms.validators import DataRequired
 import gspread
 import pandas as pd
 
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'you-will-never-guess'
-
 # Geting the Forms ready to be used
 class LoginForm(FlaskForm):
     email = StringField('Email Address', validators=[DataRequired()])
     submit = SubmitField('Find me...')
-def update_gsheet_df(usernr, labname,progress):
+def update_gsheet_df(usernr, lab,progress):
     # Based o the information we got we need to set some variables to the correct values.
     row = int(usernr) + 1
-    if "euc" in labname.lower():
-        col = 16  # Column AA
-        df_col = "EUC"
-        web_templ = "web_euc_form.html"
-
-    elif "cloud" in labname.lower():
-        col = 17  # Column AB
-        df_col = "CloudNative"
-        web_templ = "web_cloud_form.html"
-
+    col = 16
+    if "iaas" in lab:  # Enter the IAAS labs
+        type_lab = lab[8:]
+        item_nr = lab_type_lst.index(type_lab)
+        col = col + item_nr
+        web_templ="web_hybrid_cloud.html"
+    elif "db" in lab:  # Enter the DB labs
+        type_lab = lab[6:]
+        item_nr = lab_type_lst.index(type_lab)
+        col = col + item_nr
+        web_templ = "web_database.html"
+    elif "euc" in lab:  # Enter the EUC labs
+        type_lab = lab[7:]
+        item_nr = lab_type_lst.index(type_lab)
+        col = col + item_nr
+        web_templ = "web_euc.html"
+    elif "cicd" in lab:  # Enter the CICD labs
+        type_lab = lab[5:]
+        item_nr = lab_type_lst.index(type_lab)
+        col = col + item_nr
+        web_templ = "web_cicd.html"
     else:
-        col = 18  # Column AC
-        df_col = "CICD"
-        web_templ = "web_cicd_form.html"
+        type_lab = lab[6:]
+        item_nr = lab_type_lst.index(type_lab)
+        col = col + item_nr  # Column AC
+        web_templ = "web_cloud.html"
 
-    # Updating the Row to In progress in Gsheet AND the main DataFrame, so we have the right info if we update the data from the GSheet1
-    # update GSheet
+    # Update Gsheet
     wks.update_cell(row, col, progress)
     # Update the DF
     df.iat[int(usernr) - 1, int(col) - 1] = progress
-    if progress=="In progress":
-        return web_templ
+
+    return web_templ
+
+# Some Flask settings
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'you-will-never-guess'
 
 # Grabbing the initial data from gsheet
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
@@ -58,6 +69,10 @@ data = wks.get_all_values()
 headers = data.pop(0)
 # Drop all data in a dataframe
 df = pd.DataFrame(data, columns=headers)
+
+# Area for definition of variables
+lab_type_lst=["snow","leap","cmdb","xplay","aav","dam","mssql","ultimate","prov","calm","flow","cont","use","era","k8s","fiesta","day2"]
+
 
 @app.route("/update")
 def update_df():
@@ -108,9 +123,23 @@ def show_form_data():
                              'karbon_start': user_info[0]['Karbon IP - Start'],
                              'karbon_stop': user_info[0]['Karbon IP - Stop'],
                              'user_x': user_info[0]['UserX'],
-                             'val_euc': user_info[0]['EUC'],
-                             'val_cloud': user_info[0]['CloudNative'],
-                             'val_cicd': user_info[0]['CICD'],
+                             'val_hc_snow': user_info[0]['hc-iaas-snow'],
+                             'val_hc_leap': user_info[0]['hc-iaas-leap'],
+                             'val_hc_cmdb': user_info[0]['hc-iaas-cmdb'],
+                             'val_hc_xplay': user_info[0]['hc-iaas-xplay'],
+                             'val_hc_db_aav': user_info[0]['hc-db-aav'],
+                             'val_hc_db_dam': user_info[0]['hc-db-dam'],
+                             'val_hc_db_mssql': user_info[0]['hc-db-mssql'],
+                             'val_hc_db_ultimate': user_info[0]['hc-db-ultimate'],
+                             'val_hc_euc_prov': user_info[0]['hc-euc-prov'],
+                             'val_hc_euc_calm': user_info[0]['hc-euc-calm'],
+                             'val_hc_euc_flow': user_info[0]['hc-euc-flow'],
+                             'val_cicd_cont': user_info[0]['cicd-cont'],
+                             'val_cicd_use': user_info[0]['cicd-use'],
+                             'val_cicd_era': user_info[0]['cicd-era'],
+                             'val_cloud_k8s': user_info[0]['cloud-k8s'],
+                             'val_cloud_fiesta': user_info[0]['cloud-fiesta'],
+                             'val_cloud_day2': user_info[0]['cloud-day2']
                              }
 
                 form.email.data=""
@@ -137,15 +166,29 @@ def show_form_validation():
                 'clusterip':clusterip,
                 'pcip': pcip
                }
-
     # Update the DF in the correct column and row
     row=int(usernr)+1
-    if "euc" in lab:
-        col=16 # Column AA
-    elif "native" in lab:
-        col=17 # Column AB
+    col = 16
+    if "iaas" in lab: # Enter the IAAS labs
+        type_lab=lab[8:]
+        item_nr=lab_type_lst.index(type_lab)
+        col=col+item_nr
+    elif "db" in lab: # Enter the DB labs
+        type_lab = lab[6:]
+        item_nr = lab_type_lst.index(type_lab)
+        col = col + item_nr
+    elif "euc" in lab: # Enter the EUC labs
+        type_lab = lab[7:]
+        item_nr = lab_type_lst.index(type_lab)
+        col = col + item_nr
+    elif "cicd" in lab: # Enter the CICD labs
+        type_lab = lab[5:]
+        item_nr = lab_type_lst.index(type_lab)
+        col = col + item_nr
     else:
-        col=18 # Column AC
+        type_lab = lab[6:]
+        item_nr = lab_type_lst.index(type_lab)
+        col = col + item_nr # Column AC
 
     # Update Gsheet
     wks.update_cell(row,col,"Pending")
@@ -183,7 +226,9 @@ def show_form_validator():
                          'clustername': dict_user['Cluster Name'],
                          'clusterip': dict_user['IP address VIP'],
                          'pc_ip':dict_user['IP address PC'],
-                         'usernr':dict_user['Nr']
+                         'usernr':dict_user['Nr'],
+                         'userx': dict_user['UserX'],
+                         'labname': labname
                         }
 
             return render_template(web_templ, title='vGTS 2021 - Cluster lookup', user=user_values)
@@ -191,45 +236,93 @@ def show_form_validator():
         else:
             # Get all users info: usernr, First Name, Last Name and Pending status lab validation, but they must not be empty!
             # Make copies of the existing big DF
-            df_val_euc=df[['Nr','First Name','Last Name','EUC']].copy()
-            df_val_cloud=df[['Nr','First Name','Last Name','CloudNative']].copy()
-            df_val_cicd=df[['Nr','First Name','Last Name','CICD']].copy()
+            df_val_hc_iaas=df[['Nr','UserX','First Name','Last Name','hc-iaas-snow','hc-iaas-leap','hc-iaas-cmdb','hc-iaas-xplay']].copy()
+            df_val_db=df[['Nr','UserX','First Name','Last Name','hc-db-aav','hc-db-dam','hc-db-mssql','hc-db-ultimate']].copy()
+            df_val_euc=df[['Nr','UserX','First Name','Last Name','hc-euc-prov','hc-euc-calm','hc-euc-flow']].copy()
+            df_val_cicd = df[['Nr','UserX', 'First Name', 'Last Name', 'cicd-cont', 'cicd-use', 'cicd-era']].copy()
+            df_val_cloud=df[['Nr','UserX','First Name','Last Name','cloud-k8s','cloud-fiesta','cloud-day2']].copy()
 
-            # Reset the index for the DFs
-            df_val_euc=df_val_euc.set_index('Nr')
-            df_val_cloud = df_val_cloud.set_index('Nr')
-            df_val_cicd = df_val_cicd.set_index('Nr')
 
             # Clean out the unneeded rows
-            df_val_euc=df_val_euc[(df_val_euc.EUC !="")]
-            df_val_cloud=df_val_cloud[(df_val_cloud.CloudNative != "")]
-            df_val_cicd=df_val_cicd[(df_val_cicd.CICD != "")]
+            df_val_hc_iaas=df_val_hc_iaas[(df_val_hc_iaas['hc-iaas-snow'] !="") | (df_val_hc_iaas['hc-iaas-leap'] !="") | (df_val_hc_iaas['hc-iaas-cmdb'] !="") | (df_val_hc_iaas['hc-iaas-xplay'] !="")]
+            df_val_db=df_val_db[(df_val_db['hc-db-aav'] != "")| (df_val_db['hc-db-dam'] != "") | (df_val_db['hc-db-mssql'] != "") | (df_val_db['hc-db-ultimate'] != "")]
+            df_val_euc=df_val_euc[(df_val_euc['hc-euc-prov']!="") | (df_val_euc['hc-euc-calm']!="") | (df_val_euc['hc-euc-flow']!="")]
+            df_val_cicd=df_val_cicd[(df_val_cicd['cicd-cont'] != "") | (df_val_cicd['cicd-use'] != "") | (df_val_cicd['cicd-era'] != "")]
+            df_val_cloud = df_val_cloud[(df_val_cloud['cloud-k8s'] != "") | (df_val_cloud['cloud-fiesta'] != "") | (df_val_cloud['cloud-day2'] != "")]
+
+            # Set new indexes on the temp DFs
+            df_val_hc_iaas=df_val_hc_iaas.set_index('Nr')
+            df_val_db=df_val_db.set_index('Nr')
+            df_val_euc=df_val_euc.set_index('Nr')
+            df_val_cicd=df_val_cicd.set_index('Nr')
+            df_val_cloud=df_val_cloud.set_index('Nr')
 
             # Create the data into a list so we can forward them to the renderer per lab
+
+            iaas_lst=[]
+            if len(df_val_hc_iaas.to_dict()['First Name']) < 1:
+                iaas_lst=[" , , , , , , , "]
+            else:
+                for key in df_val_hc_iaas.to_dict()['First Name']:
+                    iaas_lst.append(
+                             str(key)+","+str(df_val_hc_iaas.to_dict()['UserX'][key])+","+str(df_val_hc_iaas.to_dict()['First Name'][key])+","+
+                             str(df_val_hc_iaas.to_dict()['Last Name'][key])+","+str(df_val_hc_iaas.to_dict()['hc-iaas-snow'][key])+","+
+                             str(df_val_hc_iaas.to_dict()['hc-iaas-leap'][key])+","+str(df_val_hc_iaas.to_dict()['hc-iaas-cmdb'][key])+","+
+                             str(df_val_hc_iaas.to_dict()['hc-iaas-xplay'][key])
+                    )
+
+            db_lst=[]
+            if len(df_val_db.to_dict()['First Name']) < 1:
+                db_lst=[" , , , , , , , "]
+            else:
+                for key in df_val_db.to_dict()['First Name']:
+                    db_lst.append(
+                        str(key)+","+str(df_val_db.to_dict()['UserX'][key])+","+str(df_val_db.to_dict()['First Name'][key])+","+
+                        str(df_val_db.to_dict()['Last Name'][key])+","+str(df_val_db.to_dict()['hc-db-aav'][key])+","+
+                        str(df_val_db.to_dict()['hc-db-dam'][key])+","+str(df_val_db.to_dict()['hc-db-mssql'][key])+","+
+                        str(df_val_db.to_dict()['hc-db-ultimate'][key])
+                    )
+
             euc_lst=[]
-            if len(df_val_euc.to_dict()['First Name']) > 1:
-                euc_lst = [" , , , "]
+            if len(df_val_euc.to_dict()['First Name']) < 1:
+                euc_lst = [" , , , , , , , "]
             else:
                 for key in df_val_euc.to_dict()['First Name']:
-                    euc_lst.append(str(key)+","+str(df_val_euc.to_dict()['First Name'][key])+","+str(df_val_euc.to_dict()['Last Name'][key])+","+str(df_val_euc.to_dict()['EUC'][key]))
+                    euc_lst.append(
+                        str(key)+","+str(df_val_euc.to_dict()['UserX'][key])+","+str(df_val_euc.to_dict()['First Name'][key])+","+
+                        str(df_val_euc.to_dict()['Last Name'][key])+","+str(df_val_euc.to_dict()['hc-euc-prov'][key])+","+
+                        str(df_val_euc.to_dict()['hc-euc-calm'][key])+","+str(df_val_euc.to_dict()['hc-euc-flow'][key])
+                    )
 
-            cloud_lst=[]
-            if len(df_val_cloud.to_dict()['First Name']) > 1:
-                cloud_lst=[" , , , "]
-            else:
-                for key in df_val_cloud.to_dict()['First Name']:
-                    cloud_lst.append(str(key)+","+str(df_val_cloud.to_dict()['First Name'][key])+","+str(df_val_cloud.to_dict()['Last Name'][key])+","+str(df_val_cloud.to_dict()['CloudNative'][key]))
 
             cicd_lst = []
-            if len(df_val_cicd.to_dict()['First Name']) > 1:
-                cicd_lst=[" , , , "]
+            if len(df_val_cicd.to_dict()['First Name']) < 1:
+                cicd_lst=[" , , , , , , "]
             else:
                 for key in df_val_cicd.to_dict()['First Name']:
                     cicd_lst.append(
-                        str(key) + "," + str(df_val_cicd.to_dict()['First Name'][key]) + "," + str(df_val_cicd.to_dict()['Last Name'][
-                            key]) + "," + str(df_val_cicd.to_dict()['CICD'][key]))
+                        str(key) + "," + str(df_val_cicd.to_dict()['UserX'][key])+","+
+                        str(df_val_cicd.to_dict()['First Name'][key]) + "," + str(df_val_cicd.to_dict()['Last Name'][key])+","+
+                        str(df_val_cicd.to_dict()['cicd-cont'][key])+","+ str(df_val_cicd.to_dict()['cicd-use'][key])+","+
+                        str(df_val_cicd.to_dict()['cicd-era'][key])
+                    )
 
-            return render_template('web_validator.html', euclist=euc_lst,cloudlist=cloud_lst,cicdlist=cicd_lst)
+            cloud_lst=[]
+            if len(df_val_cloud.to_dict()['First Name']) < 1:
+                cloud_lst=[" , , , , , "]
+            else:
+                for key in df_val_cloud.to_dict()['First Name']:
+                    cloud_lst.append(
+                        str(key)+","+str(df_val_cloud['UserX'][key])+","+str(df_val_cloud.to_dict()['First Name'][key])+","+
+                        str(df_val_cloud.to_dict()['Last Name'][key])+","+str(df_val_cloud.to_dict()['cloud-k8s'][key])+","+
+                        str(df_val_cloud.to_dict()['cloud-fiesta'][key])+","+str(df_val_cloud.to_dict()['cloud-day2'][key])
+
+                    )
+
+            print(iaas_lst)
+
+            # Render the pages
+            return render_template('web_validator.html', iaaslist=iaas_lst,dblist=db_lst,euclist=euc_lst,cicdlist=cicd_lst,cloudlist=cloud_lst)
 
 if __name__ == "main":
     # start the app
