8a286861d011849aa7b2254b5b4d840e17ad812c
Willem Essenstam <31310808+wessenstam@users.noreply.github.com>
2021-03-23 09:58:55 +0100

    Updated the /update route to have correct updates

--- app.py
+++ app.py
@@ -163,7 +163,8 @@ def update_df():
     data = wks.get_all_values()
     headers = data.pop(0)
     # Drop all data in a dataframe for the attendees
-    df.update(pd.DataFrame(data, columns=headers))
+    global df
+    df = pd.DataFrame(data, columns=headers)
     # Clean up the lines with no email address
     df.drop(df[df['Email'] == ""].index, inplace=True)
     # ****************************************************************************************************************
@@ -172,7 +173,8 @@ def update_df():
     data = wks_sme.get_all_values()
     headers = data.pop(0)
     # Drop all data in a dataframe for the SMEs
-    df_sme.update(pd.DataFrame(data, columns=headers))
+    global df_sme
+    df_sme = pd.DataFrame(data, columns=headers)
     # Cleaning up the lines that have no name
     df_sme.drop(df_sme[df_sme['Name'] == ""].index, inplace=True)
     
